7428580629f8cc23889f9588b4b65a92e93b6030
Kevin <kevin.moya1301@gmail.com>
2021-12-10 02:15:52 -0600

    Modificado: Mejoras de legibilidad en el codigo fuente

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
-from telegram.ext import Updater, CommandHandler
 
+########### ==> [Aqui se importa de telegram.ext los elementos CommandHandler y updater] <==##########
+
+from telegram.ext import CommandHandler, Updater
+
+######################################################################################################
+
+
+############################### ==> [Definiendo funciones] <== ########################################
 def start(update, context):
-    update.message.reply_text('Funciona!!')
+    updater.text.reply_message('Hola, sea bienvenido...')
 
 
 
-
-
-
+######################################################################################################
 if __name__ == '__main__':
 
     updater = Updater(token='5084498276:AAF0FdHkXOSqOJw1VrzwiBiUF1tt3Lg2iUQ', use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler('start', start))
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
     updater.start_polling()
     updater.idle()
+#######################################################################################################
