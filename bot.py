1ac84f836dd15098501bd68760fe5716e62f7115
Kevin <kevin.moya1301@gmail.com>
2021-12-14 03:56:21 -0600

    Modificado: Mejoras de legibilidad en el codigo fuente Modificado: Mensaje al introducir el comando /start

--- bot.py
+++ bot.py
@@ -1,23 +1,18 @@
-
-########### ==> [Aqui se importa de telegram.ext los elementos CommandHandler y updater] <==##########
-
-from telegram.ext import CommandHandler, Updater
-
-######################################################################################################
+##### Zona de importaciones #####
+from telegram.ext import Updater, CommandHandler
 
 
-############################### ==> [Definiendo funciones] <== ########################################
+### Zona de variables ################
 def start(update, context):
-    updater.text.reply_message('Hola, sea bienvenido...')
+    update.message.reply_text(
+        'Hola, bienvenido a este bot de prueba creado por @KevinDM... '
+        '\n\n ¿Qué desea hacer? \n\n Generar nuevo código QR - /QR')
 
 
-
-######################################################################################################
 if __name__ == '__main__':
-
     updater = Updater(token='5084498276:AAF0FdHkXOSqOJw1VrzwiBiUF1tt3Lg2iUQ', use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
     updater.start_polling()
     updater.idle()
-#######################################################################################################
